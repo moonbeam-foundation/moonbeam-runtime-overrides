d3ca8d9ac65f83d8501bff77595c4aa3574372ed
librelois <c@elo.tf>
2021-10-21 23:38:49 +0200

    CI: second attempt

---
+++ scripts/check-tracing-runtime.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CHAINS=(
+    moonbase
+    moonriver
+)
+
+VERSION=$1
+
+#./script/build-tracing-runtime.sh $VERSION
+
+CMD="git diff --name-only"
+
+stdbuf -oL $CMD | {
+  while IFS= read -r line; do
+    echo ║ $line
+    for CHAIN in ${CHAINS[@]}; do
+      if [[ "$line" == "wasm/$CHAIN-runtime-$VERSION-substitute-tracing.wasm" ]]; then 
+        echo "Runtime $CHAIN-$VERSION as changed, please rebuild it locally with command './script/build-tracing-runtime.sh $VERSION'."
+        exit 1
+      fi
+    done
+    echo "Runtime $CHAIN-$VERSION is valid."
+  done
+}
--- scripts/exec-each-rust-workspace.sh
+++ scripts/exec-each-rust-workspace.sh
@@ -6,7 +6,7 @@ for D in *; do
     if [ -d "${D}" ]; then
         echo "Exec command '$1' on tracing/${D}"
         cd ${D}
-        $1
+        $1 || exit 1
         cd ..
     fi
 done
