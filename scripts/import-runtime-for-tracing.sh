5c5f8ca9fc84ff491168953edcd2b703ed7590d1
librelois <c@elo.tf>
2021-10-26 17:48:32 +0200

    create script to auto import runtimes

---
+++ scripts/import-runtime-for-tracing.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+CHAINS=(
+    moonbase
+    moonriver
+)
+
+CRATES_PATHS=(
+  primitives\\/account\\/
+  core-primitives
+  pallets\\/ethereum-chain-id
+  pallets\\/parachain-staking
+  precompiles\\/parachain-staking
+  pallets\\/maintenance-mode
+  pallets\\/author-mapping
+  precompiles\\/utils
+  precompiles\\/crowdloan-rewards
+  primitives\\/rpc\\/txpool
+)
+
+SPEC_VERSION=$1
+GIT_REF=${2:-"runtime-$SPEC_VERSION"}
+
+# Get moonbeam repository shopshot
+#mkdir tmp
+#git clone https://github.com/PureStake/moonbeam -b $GIT_REF tmp/moonbeam
+
+# Copy relevant files
+mkdir -p tracing/$SPEC_VERSION/primitives/rpc
+cp tmp/moonbeam/Cargo.lock tracing/$SPEC_VERSION/Cargo.lock
+cp tmp/moonbeam/rust-toolchain tracing/$SPEC_VERSION/rust-toolchain
+cp -r tmp/moonbeam/primitives/ext tracing/$SPEC_VERSION/primitives
+cp -r tmp/moonbeam/primitives/rpc/{debug,evm-tracing-events} tracing/$SPEC_VERSION/primitives/rpc
+cp -r tmp/moonbeam/runtime tracing/$SPEC_VERSION
+
+# Remove unused moonbeam runtime
+# TODO: revert theses lines when moonbeam will be in production
+rm -rf tracing/$SPEC_VERSION/runtime/moonbeam 
+
+# Create a virtual manifest for this new rust workspace from tracing template
+cp tracing/Cargo.toml.template tracing/$SPEC_VERSION/Cargo.toml
+
+# Set evm branch
+EVM_BRANCH="runtime-$SPEC_VERSION-substitute-tracing"
+sed -i -e "s/EVM_BRANCH/$EVM_BRANCH/g" tracing/$SPEC_VERSION/Cargo.toml
+
+# For each runtime
+for CHAIN in ${CHAINS[@]}; do
+  # Enable evm-tracing feature
+  sed -i -e 's/\["std"\]/\["std", "evm-tracing"\]/g' tracing/$SPEC_VERSION/runtime/$CHAIN/Cargo.toml
+
+  # Replace path dependencies by git dependencies
+  for CRATE_PATH in ${CRATES_PATHS[@]}; do
+    echo $CRATE_PATH
+    sed -i -e "s/path = \"..\/..\/$CRATE_PATH\"/git = \"https:\/\/github.com\/purestake\/moonbeam\", rev = \"runtime-$SPEC_VERSION\"/g" tracing/$SPEC_VERSION/runtime/$CHAIN/Cargo.toml
+  done
+done
