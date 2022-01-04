30b442e6ce5f6f1d5d8e6db83995591c784415f3
librelois <c@elo.tf>
2022-01-04 14:52:22 +0100

    add filter param to script build-tracing-runtime

--- scripts/build-tracing-runtime.sh
+++ scripts/build-tracing-runtime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Constants
-CHAINS=(
+ALL_RUNTIMES_NAMES=(
     moonbase
     moonriver
     moonbeam
@@ -10,6 +10,7 @@ SRTOOL_IMAGE="paritytech/srtool:1.53.0"
 
 # Arguments
 VERSION=$1
+RUNTIME_NAME_FILTER=${2:-"[a-z]"}
 
 # Download srtool image
 docker pull $SRTOOL_IMAGE
@@ -23,27 +24,29 @@ cd tmp/build/tracing/${VERSION}
 # Move all dependencies to shared (to be in the rust workspace)
 find . -path './target' -prune -o  -name '*.toml' -exec sed -i 's/..\/..\/shared/..\/shared/g' {} \;
 
-for CHAIN in ${CHAINS[@]}; do
-  RUNTIME_DIR="runtime/$CHAIN"
-  if [ -d "$RUNTIME_DIR" ]; then
-    echo "Build $CHAIN-${VERSION}-substitute-tracing…"
+for RUNTIME_NAME in ${ALL_RUNTIMES_NAMES[@]}; do
+  RUNTIME_DIR="runtime/$RUNTIME_NAME"
+  if [[ "$RUNTIME_NAME" =~ $RUNTIME_NAME_FILTER ]]; then
+    if [ -d "$RUNTIME_DIR" ]; then
+      echo "Build $RUNTIME_NAME-${VERSION}-substitute-tracing…"
 
-    CMD="docker run -i --rm -e PACKAGE=$CHAIN-runtime -e RUNTIME_DIR=$RUNTIME_DIR -v $PWD:/build $SRTOOL_IMAGE build --app --json -cM"
+      CMD="docker run -i --rm -e PACKAGE=$RUNTIME_NAME-runtime -e RUNTIME_DIR=$RUNTIME_DIR -v $PWD:/build $SRTOOL_IMAGE build --app --json -cM"
 
-    # here we keep streaming the progress and fetch the last line for the json result
-    stdbuf -oL $CMD | {
-      while IFS= read -r line
-      do
-        echo ║ $line
-        JSON="$line"
-      done
-      # Copy wasm blob and josn digest in git repository
-      Z_WASM=`echo $JSON | jq -r .runtimes.compressed.wasm`
-      cp $Z_WASM ../../../../wasm/$CHAIN-runtime-$VERSION-substitute-tracing.wasm
-      echo $JSON > ../../../../srtool-digest/$CHAIN-runtime-$VERSION-substitute-tracing.json
-    }
+      # here we keep streaming the progress and fetch the last line for the json result
+      stdbuf -oL $CMD | {
+        while IFS= read -r line
+        do
+          echo ║ $line
+          JSON="$line"
+        done
+        # Copy wasm blob and josn digest in git repository
+        Z_WASM=`echo $JSON | jq -r .runtimes.compressed.wasm`
+        cp $Z_WASM ../../../../wasm/$RUNTIME_NAME-runtime-$VERSION-substitute-tracing.wasm
+        echo $JSON > ../../../../srtool-digest/$RUNTIME_NAME-runtime-$VERSION-substitute-tracing.json
+      }
 
-    echo "Finished building $CHAIN-$VERSION-substitute-tracing"
+      echo "Finished building $RUNTIME_NAME-$VERSION-substitute-tracing"
+    fi
   fi
 done
 
