f3f6db2a20bc6b1915dc32b395d06b9107f9d22d
nanocryk <6422796+nanocryk@users.noreply.github.com>
2023-02-22 17:00:08 +0100

    migrate tracing 2200 to workspace deps

--- scripts/update-tracing-runtime-lock.sh
+++ scripts/update-tracing-runtime-lock.sh
@@ -7,7 +7,7 @@ echo "Copying shared directory in tracing/${VERSION}"
 cp -r tracing/shared tracing/${VERSION}/shared
 
 cd tracing/${VERSION}
-echo "Running `cargo fetch` to update the lock file"
+echo "Running 'cargo fetch' to update the lock file"
 
 echo
 cargo fetch
