d703361b59f55286817eb00cf114880bd8c4138a
tgmichel <telmo@purestake.com>
2023-05-22 09:49:21 +0200

    Add `trace_call` support + update latest runtime

--- tracing/shared/primitives/rpc/debug/src/lib.rs
+++ tracing/shared/primitives/rpc/debug/src/lib.rs
@@ -25,7 +25,7 @@ use ethereum::TransactionV0 as Transaction;
 #[cfg(all(not(feature = "before_700"), not(feature = "_700_to_1200")))]
 use ethereum::TransactionV2 as Transaction;
 
-use ethereum_types::H256;
+use ethereum_types::{H160, H256, U256};
 use sp_std::vec::Vec;
 
 #[cfg(all(not(feature = "before_700"), not(feature = "_700_to_1200")))]
@@ -41,6 +41,18 @@ sp_api::decl_runtime_apis! {
 			extrinsics: Vec<Block::Extrinsic>,
 			known_transactions: Vec<H256>,
 		) -> Result<(), sp_runtime::DispatchError>;
+
+		fn trace_call(
+			from: H160,
+			to: H160,
+			data: Vec<u8>,
+			value: U256,
+			gas_limit: U256,
+			max_fee_per_gas: Option<U256>,
+			max_priority_fee_per_gas: Option<U256>,
+			nonce: Option<U256>,
+			access_list: Option<Vec<(H160, Vec<H256>)>>,
+		) -> Result<(), sp_runtime::DispatchError>;
 	}
 }
 
@@ -55,6 +67,18 @@ sp_api::decl_runtime_apis! {
 		fn trace_block(
 			extrinsics: Vec<Block::Extrinsic>,
 			known_transactions: Vec<H256>,
+		) -> Result<(), sp_runtime::DispatchError>;
+
+		fn trace_call(
+			from: H160,
+			to: H160,
+			data: Vec<u8>,
+			value: U256,
+			gas_limit: U256,
+			max_fee_per_gas: Option<U256>,
+			max_priority_fee_per_gas: Option<U256>,
+			nonce: Option<U256>,
+			access_list: Option<Vec<(H160, Vec<H256>)>>,
 		) -> Result<(), sp_runtime::DispatchError>;
 	}
 }
