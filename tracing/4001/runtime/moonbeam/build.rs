1f18829c125bb64092e2c9d2136caaa3a08b6695
Manuel Mauro <manuel.mauro@protonmail.com>
2025-11-18 16:46:40 +0200

    Add tracing for runtime-4001 (#147)
    * Add tracing sources for runtime-4001
    
    * Add tracing WASM for runtime-4001
    
    * Add tracing sources for runtime-4001
    
    * Add tracing WASM for runtime-4001

---
+++ tracing/4001/runtime/moonbeam/build.rs
@@ -0,0 +1,30 @@
+// Copyright 2019-2025 PureStake Inc.
+// This file is part of Moonbeam.
+
+// Moonbeam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonbeam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+
+#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
+fn main() {
+	substrate_wasm_builder::WasmBuilder::init_with_defaults().build()
+}
+
+#[cfg(all(feature = "std", feature = "metadata-hash"))]
+fn main() {
+	substrate_wasm_builder::WasmBuilder::init_with_defaults()
+		.enable_metadata_hash("GLMR", 18)
+		.build()
+}
+
+#[cfg(not(feature = "std"))]
+fn main() {}
